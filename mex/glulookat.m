5f705157ea2e523b81d88f80f8a53ff5e8f9a094
Steve132 <steve1@soapforge.com>
2018-09-03 14:53:14 -0400

    Add glu simulation functions to interface camera matrices based on matlab

---
+++ mex/glulookat.m
@@ -0,0 +1,19 @@
+function [P]=gluplookat(pos,target,up)
+
+pos=pos(:);
+target=target(:);
+up=up(:);
+
+f=target-pos;
+f=f./norm(f);
+up=up./norm(up);
+
+s=cross(f,up);
+u=cross(s./norm(s),f);
+
+P=eye(4);
+P(1,1:3)=s';
+P(2,1:3)=u';
+P(3,1:3)=-f';
+
+end
---
+++ mex/gluperspective.m
@@ -0,0 +1,11 @@
+function [P]=gluperspective(fovy,aspect,near,far)
+
+f=cot(fovy/2.0);
+P=zeros(4);
+P(1,1)=f/aspect;
+P(2,2)=f;
+P(3,3)=(far+near)/(near-far);
+P(3,4)=(far*near*2.0)/(near-far);
+P(4,3)=-1.0;
+
+end
